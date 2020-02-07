1d71ca8924914bf659a36d51d8f7e034cc1b6bf0
Madhav <madhavkhanna2021@u.northwestern.edu>
2020-02-07 14:49:00 -0600

    Added ParticleSys.js

---
+++ Code/PartSysType.js
@@ -0,0 +1,19 @@
+/* 
+ * This file contains the type of particle systems available which
+ * will be used in the program to switch between different constraints and
+ * effects on the particles
+ */
+
+const BOUNCY_BALL = 0;
+const FOUNTAIN = 1;
+const FIRE = 2;
+const SPRING_PAIR = 3;
+const CLOTH = 4;
+const TORNADO = 5;
+const SPRING_ROPE = 6;
+const SPRING_SOLID = 7;
+
+//Constructor
+function PartSysType() {
+    this.PartSysType = BOUNCY_BALL;
+}
